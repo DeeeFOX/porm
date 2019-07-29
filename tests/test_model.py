1dbffde940e4fb5e339efefb3ae3532af90978a2
DeeeFOX <dennias.chiu@gmail.com>
2019-07-30 01:31:14 +0800

    Update test_model.py
    more useful test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,6 @@
 from porm import IntegerType, VarcharType, TextType, DatetimeType
 from porm.databases.api.mysql import CONN_CONF
 from porm.model import DBModel
-from tests.context import db_loader
 from tests.test_common import DatabaseTestCase
 
 
@@ -21,24 +20,21 @@ class TestDatabase(DatabaseTestCase):
         user_info = self.UserInfo.new(email='dennias.chiu@gmail.com', username='dennias')
         with user_info.dbi.start_transaction() as _t:
             user_info.dbi.execute_sql("""
-                    CREATE TABLE `UserInfo` (
-                      `userid` bigint NOT NULL AUTO_INCREMENT,
-                      `email` varchar(255) NOT NULL COMMENT '电子邮箱',
-                      `username` varchar(255) NOT NULL COMMENT '用户名',
-                      `descr` text DEFAULT NULL COMMENT '用户说明',
-                      `createtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
-                      `updatetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
-                      `is_active` tinyint(1) DEFAULT '1' COMMENT '用户有效标志',
-                      PRIMARY KEY (`userid`),
-                      UNIQUE KEY `email` (`email`),
-                      UNIQUE KEY `username` (`username`)
-                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
+            CREATE TABLE `UserInfo` (
+              `userid` bigint NOT NULL AUTO_INCREMENT,
+              `email` varchar(255) NOT NULL COMMENT '电子邮箱',
+              `username` varchar(255) NOT NULL COMMENT '用户名',
+              `descr` text DEFAULT NULL COMMENT '用户说明',
+              `createtime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+              `updatetime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+              `is_active` tinyint(1) DEFAULT '1' COMMENT '用户有效标志',
+              PRIMARY KEY (`userid`),
+              UNIQUE KEY `email` (`email`),
+              UNIQUE KEY `username` (`username`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8;""")
             user_info.insert(t=_t)
             obj = self.UserInfo.get_one(email='dennias.chiu@gmail.com', username='dennias', t=_t)
             self.assertIsNotNone(obj)
 
-    # def test_drop_table(self):
-    #     self.api.execute_sql('DROP TABLE UserInfo;')
-    #
-    # def test_drop_database(self):
-    #     self.api.execute_sql('DROP DATABASE PORM_DATABASE;')
+    def test_drop_table(self):
+        self.api.execute_sql('DROP TABLE UserInfo;')
